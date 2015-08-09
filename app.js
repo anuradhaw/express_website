ef1307d14e04cd18bad9c9d32ee9525cb29c9ef3
Anu <anurada914@gmail.com>
2015-08-09 11:28:31 +0930

    more css and content edits

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ router.get('/about', function(req,res){
   });
 })
 
+
+
 /**********catch 404 and forward to error handler ************/
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ public/javascripts/activelink.js
@@ -0,0 +1,9 @@
+var $ = require('jquery');
+
+$(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
+            e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });
